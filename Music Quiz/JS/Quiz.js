1ccc67a7a0dcb206704acaa81946d5bb9a85d92c
Thieu Tu <tu.thieuthanh@gmail.com>
2018-03-23 16:35:24 +0700

    Edit Quiz page

---
+++ Music Quiz/JS/Quiz.js
@@ -0,0 +1,13 @@
+function trans_quizpage(){
+	window.location.href = 'Quiz.html'
+};
+
+var question =[ 
+ques_1={content: 'Tên bài hát bạn đang nghe có tên là gì?', A: 'Vỡ', B: 'Cùng anh', C: 'Buồn của anh', D: 'Đừng xin lỗi nữa'},
+ques_2={content: 'Tên bài hát bạn đang nghe có tên là gì?', A: 'Cùng anh', B: 'Em ngày xưa khác rồi', C: 'Buồn của anh', D: 'Đừng xin lỗi nữa'},
+ques_3={content: 'Tên bài hát bạn đang nghe có tên là gì?', A: 'Cùng anh', B: 'Em ngày xưa khác rồi', C: 'Đừng ai nhắc về anh ấy', D: 'Đừng xin lỗi nữa'},
+ques_4={content: 'Tên bài hát bạn đang nghe có tên là gì?', A: 'Buồn của anh', B: 'Em ngày xưa khác rồi', C: 'Buồn của anh', D: 'Đừng xin lỗi nữa'},
+ques_5={content: 'Tên bài hát bạn đang nghe có tên là gì?', A: 'Cùng anh', B: 'Em ngày xưa khác rồi', C: 'Buồn của anh', D: 'Đừng xin lỗi nữa'},
+ques_6={content: 'Tên bài hát bạn đang nghe có tên là gì?', A: 'Cùng anh', B: 'Em ngày xưa khác rồi', C: 'Buồn của anh', D: 'Vỡ'},
+
+];
